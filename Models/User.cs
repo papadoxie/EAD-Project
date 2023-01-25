9eb540ae8acc99ea89dc80d93726c3cdc081e3ad
papadoxie <nofilqasim@gmail.com>
2023-01-25 21:53:35 +0500

    Updated PK Field to conform to Entity Framework

--- Models/User.cs
+++ Models/User.cs
@@ -3,6 +3,7 @@
     public class User
     {
         // User identification attributes
+        public int ID { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; }
         public string? Email { get; set; }
