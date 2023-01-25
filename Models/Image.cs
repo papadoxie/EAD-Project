155f87d125dbfad6ddf72a7254e4100a7eb5195e
papadoxie <nofilqasim@gmail.com>
2023-01-25 21:33:05 +0500

    Added Docker Image Model

---
+++ Models/Image.cs
@@ -0,0 +1,11 @@
+﻿namespace EAD_Project.Models
+{
+    public class Image
+    {
+        public string? Name { get; set; }
+        public string? Tag { get; set; }
+        public string? Id { get; set; }
+        public string? Created { get; set; }
+        
+    }
+}
