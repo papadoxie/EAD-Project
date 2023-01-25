5287a4a364db54a73919f0672fd952ce9656e23a
WaleedButt2 <waleedb403@gmail.com>
2023-01-25 22:21:58 +0500

    Sign in Page

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,16 @@
-﻿namespace EAD_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace EAD_Project.Models
 {
     public class User
     {
-        // User identification attributes
+    // User identification attributes
+        [Required(ErrorMessage = "UserName is required")]
         public string? Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
         public string? Password { get; set; }
-        public string? Email { get; set; }
-
+        [Required(ErrorMessage = "Email is required")]
+         public string? Email { get; set; }
         // User resources on PUCCI
         public List<Container>? Containers { get; set; }
     }
