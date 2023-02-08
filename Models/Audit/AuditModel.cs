00ec9c165c0acdc666bf81cb7cc2891b0fa91166
papadoxie <nofilqasim@gmail.com>
2023-02-09 02:53:42 +0500

    Line ending char has changed for some reason

--- Models/Audit/AuditModel.cs
+++ Models/Audit/AuditModel.cs
@@ -1,8 +1,8 @@
 ﻿namespace PUCCI.Models.Audit
 {
-    public interface AuditModel
+    public abstract class AuditModel
     {
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
         public DateTime CreationTime { get; set; }
         public DateTime ModifiedTime { get; set; }
         public bool IsDeleted { get; set; }
