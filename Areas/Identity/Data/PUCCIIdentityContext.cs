74349283a150244728841dd97180c033dacca8a0
papadoxie <nofilqasim@gmail.com>
2023-02-09 02:46:06 +0500

    Add auditing information on save changes

--- Areas/Identity/Data/PUCCIIdentityContext.cs
+++ Areas/Identity/Data/PUCCIIdentityContext.cs
@@ -1,24 +1,59 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using PUCCI.Areas.Identity.Data;
+using PUCCI.Models.Audit;
 
 namespace PUCCI.Data;
 
 public partial class PUCCIIdentityContext : IdentityDbContext<User>
 {
-    public PUCCIIdentityContext(DbContextOptions<PUCCIIdentityContext> options)
+    public PUCCIIdentityContext(DbContextOptions<PUCCIIdentityContext> options, ICurrentUserService currentUserService)
         : base(options)
     {
+        _currentUserService = currentUserService;
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
+		Database.Migrate();
 
-        // Seed the DB with initial Entities
-        // DbInitializer.Seed(builder);
+		// Seed the DB with initial Entities
+		// DbInitializer.Seed(builder);
+	}
+
+	public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return base.SaveChangesAsync(cancellationToken);
     }
+
+    /// <summary>
+    /// Add auditing information before saving changes to DB
+    /// </summary>
+    public void Audit()
+    {
+        var entries = ChangeTracker.Entries();
+
+        foreach (var entry in entries)
+        {
+            if (entry.Entity is AuditModel entity)
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entity.CreationTime = DateTime.Now;
+                        entity.ModifiedTime = entity.CreationTime;
+                        entity.CreatedBy = _currentUserService.GetCurrentUsername();
+                        break;
+                    case EntityState.Modified:
+                        entity.ModifiedTime = DateTime.Now;
+                        break;
+                }
+            }
+        }
+    }
+
+    // Needed to get the User who made changes
+	private readonly ICurrentUserService _currentUserService;
 }
