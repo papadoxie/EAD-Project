5c5ef505c1ab02ea3e0cd1c0d3464ea0c4f5b8ef
papadoxie <nofilqasim@gmail.com>
2023-02-09 02:45:03 +0500

    Migrated Audit Model

--- Migrations/20230208214042_AuditModel.Designer.cs
+++ Migrations/20230208214042_AuditModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PUCCI.Data;
 
@@ -11,9 +12,11 @@ using PUCCI.Data;
 namespace PUCCI.Migrations
 {
     [DbContext(typeof(PUCCIIdentityContext))]
-    partial class PUCCIIdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20230208214042_AuditModel")]
+    partial class AuditModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
