10b9b4860c8434abff2d689e5c9d52eb6cf2d486
papadoxie <nofilqasim@gmail.com>
2023-03-03 05:06:20 +0500

    Added UserId field

--- Models/Container.cs
+++ Models/Container.cs
@@ -5,6 +5,7 @@ namespace PUCCI.Models
     public class Container : AuditModel
     {
         public string? ID { get; set; }
+        public string? UserId { get; set; }
         public string? Name { get; set; }
         public string? Image { get; set; }
         public string? Status { get; set; }
