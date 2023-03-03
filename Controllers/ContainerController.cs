07875c000a4ffe35452047b62ddb20d8542ae7c7
papadoxie <nofilqasim@gmail.com>
2023-03-03 05:07:34 +0500

    Restricted listing to show only logged in users objects

--- Controllers/ContainerController.cs
+++ Controllers/ContainerController.cs
@@ -1,163 +1,181 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
+using PUCCI.Areas.Identity.Data;
 using PUCCI.Data;
 using PUCCI.Models;
 
 namespace PUCCI.Controllers
 {
-    public class ContainerController : Controller
-    {
-        private readonly PUCCIIdentityContext _context;
+	public class ContainerController : Controller
+	{
+		private readonly PUCCIIdentityContext _context;
 
-        public ContainerController(PUCCIIdentityContext context)
-        {
-            _context = context;
-        }
+		public ContainerController(PUCCIIdentityContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Container
-        public async Task<IActionResult> Index()
-        {
-              return _context.Containers != null ? 
-                          View(await _context.Containers.ToListAsync()) :
-                          Problem("Entity set 'PUCCIIdentityContext.Containers'  is null.");
-        }
+		// GET: Container
+		[Authorize]
+		public async Task<IActionResult> Index()
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			var containers = from ctr in _context.Containers
+							 where ctr.UserId == userId
+							 select ctr;
 
-        // GET: Container/Details/5
-        public async Task<IActionResult> Details(string id)
-        {
-            if (id == null || _context.Containers == null)
-            {
-                return NotFound();
-            }
+			return _context.Containers != null ?
+				View(await containers.ToListAsync()) :
+				Problem("Entity set 'PUCCIIdentityContext.Containers'  is null.");
+		}
 
-            var container = await _context.Containers
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (container == null)
-            {
-                return NotFound();
-            }
+		// GET: Container/Details/5
+		[Authorize]
+		public async Task<IActionResult> Details(string id)
+		{
+			if (id == null || _context.Containers == null)
+			{
+				return NotFound();
+			}
 
-            return View(container);
-        }
+			var container = await _context.Containers
+				.FirstOrDefaultAsync(m => m.ID == id);
+			if (container == null)
+			{
+				return NotFound();
+			}
 
-        // GET: Container/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+			return View(container);
+		}
 
-        // POST: Container/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,Image,Status")] Container container)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(container);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(container);
-        }
+		// GET: Container/Create
+		[Authorize]
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // GET: Container/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null || _context.Containers == null)
-            {
-                return NotFound();
-            }
+		// POST: Container/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[Authorize]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("ID,Name,Image,Status")] Container container)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(container);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(container);
+		}
 
-            var container = await _context.Containers.FindAsync(id);
-            if (container == null)
-            {
-                return NotFound();
-            }
-            return View(container);
-        }
+		// GET: Container/Edit/5
+		[Authorize]
+		public async Task<IActionResult> Edit(string id)
+		{
+			if (id == null || _context.Containers == null)
+			{
+				return NotFound();
+			}
 
-        // POST: Container/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("ID,Name,Image,Status")] Container container)
-        {
-            if (id != container.ID)
-            {
-                return NotFound();
-            }
+			var container = await _context.Containers.FindAsync(id);
+			if (container == null)
+			{
+				return NotFound();
+			}
+			return View(container);
+		}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(container);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ContainerExists(container.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(container);
-        }
+		// POST: Container/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[Authorize]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(string id, [Bind("ID,Name,Image,Status")] Container container)
+		{
+			if (id != container.ID)
+			{
+				return NotFound();
+			}
 
-        // GET: Container/Delete/5
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null || _context.Containers == null)
-            {
-                return NotFound();
-            }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(container);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ContainerExists(container.ID))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(container);
+		}
 
-            var container = await _context.Containers
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (container == null)
-            {
-                return NotFound();
-            }
+		// GET: Container/Delete/5
+		[Authorize]
+		public async Task<IActionResult> Delete(string id)
+		{
+			if (id == null || _context.Containers == null)
+			{
+				return NotFound();
+			}
 
-            return View(container);
-        }
+			var container = await _context.Containers
+				.FirstOrDefaultAsync(m => m.ID == id);
+			if (container == null)
+			{
+				return NotFound();
+			}
 
-        // POST: Container/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            if (_context.Containers == null)
-            {
-                return Problem("Entity set 'PUCCIIdentityContext.Containers'  is null.");
-            }
-            var container = await _context.Containers.FindAsync(id);
-            if (container != null)
-            {
-                _context.Containers.Remove(container);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+			return View(container);
+		}
 
-        private bool ContainerExists(string id)
-        {
-          return (_context.Containers?.Any(e => e.ID == id)).GetValueOrDefault();
-        }
-    }
+		// POST: Container/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[Authorize]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(string id)
+		{
+			if (_context.Containers == null)
+			{
+				return Problem("Entity set 'PUCCIIdentityContext.Containers'  is null.");
+			}
+			var container = await _context.Containers.FindAsync(id);
+			if (container != null)
+			{
+				_context.Containers.Remove(container);
+			}
+
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool ContainerExists(string id)
+		{
+			return (_context.Containers?.Any(e => e.ID == id)).GetValueOrDefault();
+		}
+	}
 }
--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -13,205 +14,216 @@ using PUCCI.Models;
 
 namespace PUCCI.Controllers
 {
-    public class ImageController : Controller
-    {
-        private readonly PUCCIIdentityContext _context;
-        
-        // Used to get filepath for saving Dockerfiles
-        private readonly IWebHostEnvironment _environment;
+	public class ImageController : Controller
+	{
+		private readonly PUCCIIdentityContext _context;
 
-        // Used to get instance of User accessing the page
-        private readonly UserManager<User> _userManager;
+		// Used to get filepath for saving Dockerfiles
+		private readonly IWebHostEnvironment _environment;
 
-        public ImageController( PUCCIIdentityContext context, 
-                                IWebHostEnvironment environment,
-                                UserManager<User> userManager)
-        {
-            _environment = environment;
-            _context = context;
-            _userManager = userManager;
-        }
+		// Used to get instance of User accessing the page
+		private readonly UserManager<User> _userManager;
 
-        // GET: Image
-        public async Task<IActionResult> Index()
-        {
-              return _context.Images != null ? 
-                          View(await _context.Images.ToListAsync()) :
-                          Problem("Entity set 'PUCCIIdentityContext.Images'  is null.");
-        }
+		public ImageController(PUCCIIdentityContext context,
+								IWebHostEnvironment environment,
+								UserManager<User> userManager)
+		{
+			_environment = environment;
+			_context = context;
+			_userManager = userManager;
+		}
 
-        // GET: Image/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Images == null)
-            {
-                return NotFound();
-            }
+		// GET: Image
+		[Authorize]
+		public async Task<IActionResult> Index()
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			var images = from img in _context.Images
+						 where img.UserId == userId
+						 select img;
+			return _context.Images != null ?
+						  View(await images.ToListAsync()) :
+						  Problem("Entity set 'PUCCIIdentityContext.Images'  is null.");
+		}
 
-            var image = await _context.Images
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (image == null)
-            {
-                return NotFound();
-            }
+		// GET: Image/Details/5
+		[Authorize]
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.Images == null)
+			{
+				return NotFound();
+			}
 
-            return View(image);
-        }
+			var image = await _context.Images
+				.FirstOrDefaultAsync(m => m.ID == id);
+			if (image == null)
+			{
+				return NotFound();
+			}
 
-        // GET: Image/Create
-        [Authorize]
-        public IActionResult Create()
-        {
-            return View();
-        }
+			return View(image);
+		}
 
-        // POST: Image/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize]
-        public async Task<IActionResult> Create([Bind("Name")] Image image, IFormFile Dockerfile)
-        {
-            if (!ModelState.IsValid || Dockerfile is null)
-            {
-                ViewBag.Message = "Error";
-		return View();
-            }
-           
-            // Save Dockerfile to webroot
-            string wwwPath = _environment.WebRootPath;
-            string path = Path.Combine(wwwPath, "Dockerfiles");
-                
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
+		// GET: Image/Create
+		[Authorize]
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-            var fileName = Path.GetFileName(Dockerfile.FileName);
-            // Keep Dockerfile name unique
-	    fileName += Guid.NewGuid();
-            var filePath = Path.Combine(path, fileName);
-                
-            using (FileStream stream = new FileStream(filePath, FileMode.Create))
-            {
-                Dockerfile.CopyTo(stream);
-            }
+		// POST: Image/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		[Authorize]
+		public async Task<IActionResult> Create([Bind("Name")] Image image, IFormFile Dockerfile)
+		{
+			if (!ModelState.IsValid || Dockerfile is null)
+			{
+				ViewBag.Message = "Error";
+				return View();
+			}
 
-            image.DockerfilePath = filePath;
+			// Save Dockerfile to webroot
+			string wwwPath = _environment.WebRootPath;
+			string path = Path.Combine(wwwPath, "Dockerfiles");
 
-            // Get User to save image in
-            var user = await _userManager.GetUserAsync(User);
+			if (!Directory.Exists(path))
+			{
+				Directory.CreateDirectory(path);
+			}
 
-            if(user is null)
-            {
-                // Replace this with a reasonable error
-                return View();
-            }
+			var fileName = Path.GetFileName(Dockerfile.FileName);
+			// Keep Dockerfile name unique
+			fileName += Guid.NewGuid();
+			var filePath = Path.Combine(path, fileName);
 
-            // Will only run if user is creating images the first time
-            if(user.Images == null)
-            {
-                user.Images = new List<Image>();
-            }            
-            user.Images.Add(image);
+			using (FileStream stream = new FileStream(filePath, FileMode.Create))
+			{
+				Dockerfile.CopyTo(stream);
+			}
 
-            await _context.SaveChangesAsync();
+			image.DockerfilePath = filePath;
 
-            return RedirectToAction(nameof(Index));
-        }
+			// Get User to save image in
+			var user = await _userManager.GetUserAsync(User);
 
-        // GET: Image/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Images == null)
-            {
-                return NotFound();
-            }
+			if (user is null)
+			{
+				// Replace this with a reasonable error
+				return View();
+			}
 
-            var image = await _context.Images.FindAsync(id);
-            if (image == null)
-            {
-                return NotFound();
-            }
-            return View(image);
-        }
+			// Will only run if user is creating images the first time
+			if (user.Images == null)
+			{
+				user.Images = new List<Image>();
+			}
+			user.Images.Add(image);
+			image.Create();
 
-        // POST: Image/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int? id, [Bind("ID,ImageID,Name,Tag,Created,DockerfilePath")] Image image)
-        {
-            if (id != image.ID)
-            {
-                return NotFound();
-            }
+			await _context.SaveChangesAsync();
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(image);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ImageExists(image.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(image);
-        }
+			return RedirectToAction(nameof(Index));
+		}
 
-        // GET: Image/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Images == null)
-            {
-                return NotFound();
-            }
+		// GET: Image/Edit/5
+		[Authorize]
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null || _context.Images == null)
+			{
+				return NotFound();
+			}
 
-            var image = await _context.Images
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (image == null)
-            {
-                return NotFound();
-            }
+			var image = await _context.Images.FindAsync(id);
+			if (image == null)
+			{
+				return NotFound();
+			}
+			return View(image);
+		}
 
-            return View(image);
-        }
+		// POST: Image/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		[Authorize]
+		public async Task<IActionResult> Edit(int? id, [Bind("ID,ImageID,Name,Tag,Created,DockerfilePath")] Image image)
+		{
+			if (id != image.ID)
+			{
+				return NotFound();
+			}
 
-        // POST: Image/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int? id)
-        {
-            if (_context.Images == null)
-            {
-                return Problem("Entity set 'PUCCIIdentityContext.Images'  is null.");
-            }
-            var image = await _context.Images.FindAsync(id);
-            if (image != null)
-            {
-                _context.Images.Remove(image);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(image);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ImageExists(image.ID))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(image);
+		}
 
-        private bool ImageExists(int? id)
-        {
-          return (_context.Images?.Any(e => e.ID == id)).GetValueOrDefault();
-        }
-    }
+		// GET: Image/Delete/5
+		[Authorize]
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null || _context.Images == null)
+			{
+				return NotFound();
+			}
+
+			var image = await _context.Images
+				.FirstOrDefaultAsync(m => m.ID == id);
+			if (image == null)
+			{
+				return NotFound();
+			}
+
+			return View(image);
+		}
+
+		// POST: Image/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		[Authorize]
+		public async Task<IActionResult> DeleteConfirmed(int? id)
+		{
+			if (_context.Images == null)
+			{
+				return Problem("Entity set 'PUCCIIdentityContext.Images'  is null.");
+			}
+			var image = await _context.Images.FindAsync(id);
+			if (image != null)
+			{
+				_context.Images.Remove(image);
+			}
+
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool ImageExists(int? id)
+		{
+			return (_context.Images?.Any(e => e.ID == id)).GetValueOrDefault();
+		}
+	}
 }
